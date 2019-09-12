d6345c08d94cc9f7e4c177425f9bf25242dbe3c9
Ivo Donchev <i.donchev@hacksoft.io>
2019-09-12 21:34:42 +0300

    Place cursor at the right place

--- plugin/src/main.py
+++ plugin/src/main.py
@@ -21,6 +21,8 @@ def fill_import():
     current_word = vim.eval('expand("<cword>")')
 
     current_buffer = vim.current.buffer
+    current_window = vim.current.window
+    cursor_current_row, cursor_current_col = current_window.cursor
 
     import_statement = get_import_statement(obj_to_import=current_word)
 
@@ -28,3 +30,4 @@ def fill_import():
         # Import at the beginning at the file
         # TODO: This is a temporary implementation... Find the exact place to put the import
         current_buffer.append(import_statement, 0)
+        current_window.cursor = (cursor_current_row + 1, cursor_current_col)
